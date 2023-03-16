2d3563952f1944b61ad9a8c5841a32366c8cfee3
Tony Li <tingfung.tony@gmail.com>
2023-03-17 00:31:26 +0800

    feat(app): initial pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+import runtimeCaching from 'next-pwa/cache.js';
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = nextConfig
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+  dest: 'public',
+  disable: !isProduction,
+  runtimeCaching,
+})({
+  ...nextConfig
+});
