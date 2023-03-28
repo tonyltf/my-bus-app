c2d62521fecf3550e41ca6cf034da183803b9fe7
Tony Li <tingfung.tony@gmail.com>
2023-03-28 17:48:22 +0000

    feat(Providers.tsx): add session prop to SessionProvider component

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,20 +1,20 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, Session } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { FC, ReactNode } from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
 
-
 interface ProvidersProps {
+  session: Session;
   children: ReactNode
 }
 
-const Providers: FC<ProvidersProps> = ({ children }) => {
+const Providers: FC<ProvidersProps> = ({ session, children }) => {
   return (
     <StyledEngineProvider injectFirst>
     <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider session={session}>{children}</SessionProvider>
     </ThemeProvider>
     </StyledEngineProvider>
   )
