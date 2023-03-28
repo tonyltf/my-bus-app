39f29d17ba8dde0fcd2dbf0ed4c068a56b424512
Tony Li <tingfung.tony@gmail.com>
2023-03-28 18:37:58 +0000

    🎨 style(Menu.tsx): import LoginModal from its own file and remove unused imports
    ♻️ refactor(Menu.tsx): extract login modal to a separate component

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -11,6 +11,7 @@ import ListItemText from '@mui/material/ListItemText';
 import { ArrowLeft, Bus, Star, Settings, LogInIcon } from 'lucide-react';
 import { Box, Fade, Modal } from '@mui/material';
 import Login from './Login';
+import LoginModal from './Login/Modal';
 
 const Menu = ({ open, setOpen }: { open: boolean, setOpen: Dispatch<SetStateAction<boolean>> }) => {
   const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
@@ -19,70 +20,58 @@ const Menu = ({ open, setOpen }: { open: boolean, setOpen: Dispatch<SetStateActi
 
   return (
     <>
-    <SwipeableDrawer
-      className='dark:bg-slate-900'
-      slotProps={{ root: (state) => ({ className: 'dark:bg-slate-900'}) }}
-      PaperProps={{
-        classes: {
-          root: 'dark:bg-slate-900 dark:text-slate-50',
-        }
-      }}
-      open={open}
-      onOpen={(event) => { setOpen(true); }}
-      onClose={(event) => { setOpen(false); }}
-    >
-      <div className='h-12 container flex items-center px-4'>
-        <ArrowLeft onClick={() => setOpen(false)} />
-      </div>
-      <Divider />
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Bus className='dark:text-slate-50'/>
-            </ListItemIcon>
-            <ListItemText primary='Transit' />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Star className='dark:text-slate-50'/>
-            </ListItemIcon>
-            <ListItemText primary='Favourite' />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Settings className='dark:text-slate-50'/>
-            </ListItemIcon>
-            <ListItemText primary='Setting' />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton onClick={() => { setOpenLoginModal(true); }}>
-            <ListItemIcon>
-              <LogInIcon className='dark:text-slate-50'/>
-            </ListItemIcon>
-            <ListItemText primary='Login' />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </SwipeableDrawer>
-    { /* Login modal */}
-    <Modal
-      open={openLoginModal}
-      onClose={() => { setOpenLoginModal(false); }}
+      <SwipeableDrawer
+        className='dark:bg-slate-900'
+        slotProps={{ root: (state) => ({ className: 'dark:bg-slate-900'}) }}
+        PaperProps={{
+          classes: {
+            root: 'dark:bg-slate-900 dark:text-slate-50',
+          }
+        }}
+        open={open}
+        onOpen={(event) => { setOpen(true); }}
+        onClose={(event) => { setOpen(false); }}
       >
-        <Fade in={openLoginModal}>
-          <Box className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/50 backdrop-blur-sm'>
-            <div className='flex justify-center items-center'>
-              <Login />
-            </div>
-          </Box>
-        </Fade>
-      </Modal>
+        <div className='h-12 container flex items-center px-4'>
+          <ArrowLeft onClick={() => setOpen(false)} />
+        </div>
+        <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Bus className='dark:text-slate-50'/>
+              </ListItemIcon>
+              <ListItemText primary='Transit' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Star className='dark:text-slate-50'/>
+              </ListItemIcon>
+              <ListItemText primary='Favourite' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Settings className='dark:text-slate-50'/>
+              </ListItemIcon>
+              <ListItemText primary='Setting' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => { setOpenLoginModal(true); }}>
+              <ListItemIcon>
+                <LogInIcon className='dark:text-slate-50'/>
+              </ListItemIcon>
+              <ListItemText primary='Login' />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+      <LoginModal openLoginModal={openLoginModal} setOpenLoginModal={setOpenLoginModal} />
     </>
   )
 }
