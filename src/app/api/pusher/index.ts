0f88d2a505fd14d4badedf61bab3644eb9f6d0f3
Tony Li <tingfung.tony@gmail.com>
2023-04-05 07:14:49 +0000

    🐛 fix(pusher/index.ts): add default empty string values for environment variables 🚧 chore(pusher/index.ts): comment out handler function and its parameters

--- src/app/api/pusher/index.ts
+++ src/app/api/pusher/index.ts
@@ -1,20 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
 import Pusher from 'pusher';
 
 export const pusher = new Pusher({
-  host: process.env.NEXT_PUSH_APP_HOST,
-  port: process.env.NEXT_PUSH_APP_PORT,
-  appId: process.env.NEXT_PUSH_APP_ID,
-  key: process.env.NEXT_PUSH_APP_KEY,
-  secret: process.env.NEXT_PUSH_APP_SECRET,
+  host: process.env.NEXT_PUSH_APP_HOST || '',
+  port: process.env.NEXT_PUSH_APP_PORT || '',
+  appId: process.env.NEXT_PUSH_APP_ID || '',
+  key: process.env.NEXT_PUSH_APP_KEY || '',
+  secret: process.env.NEXT_PUSH_APP_SECRET || '',
   useTLS: process.env.NEXT_PUSH_APP_TLS === 'true',
 });
 
-export default async function handler(req, res) {
-  const { message, sender } = req.body;
-  const response = await pusher.trigger("chat", "chat-event", {
-    message,
-    sender,
-  });
+// export default async function handler(req: NextRequest, res: NextResponse) {
+//   const { message, sender } = req.body as { message: string; sender: string };
+//   const response = await pusher.trigger("chat", "chat-event", {
+//     message,
+//     sender,
+//   });
 
-  res.json({ message: "completed" });
-}
+//   res.json({ message: "completed" });
+// }
